3cf64f602fefca27a03b37754034fa59907b0c6f
guanghui.chen <guanghui.chen@thoughtworks.com>
2025-03-16 22:08:17 +0800

    实现method inject功能

--- di.contanier/src/main/java/geektime/tdd/di/ConstructorInjectionProvider.java
+++ di.contanier/src/main/java/geektime/tdd/di/ConstructorInjectionProvider.java
@@ -5,21 +5,28 @@ import jakarta.inject.Inject;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
+import static java.util.stream.Stream.concat;
 
 class ConstructorInjectionProvider<Type> implements ContextConfig.ComponentProvider<Type> {
     private Constructor<Type> injectionConstructor;
     private List<Field> injectionFields;
+    private List<Method> injectionMethods;
 
     public ConstructorInjectionProvider(Class<Type> component) {
         this.injectionConstructor = getInjectConstructor(component);
         this.injectionFields = getInjectionFields(component);
+        this.injectionMethods = getInjectionMethods(component);
     }
+
 
     @Override
     public Type get(Context context) {
@@ -33,6 +40,10 @@ class ConstructorInjectionProvider<Type> implements ContextConfig.ComponentProvi
                 field.setAccessible(true);
                 field.set(instance, context.get(field.getType()).get());
             }
+            for (Method method : injectionMethods) {
+                method.setAccessible(true);
+                method.invoke(instance, stream(method.getParameters()).map(p -> context.get(p.getType()).get()).toArray());
+            }
             return instance;
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
@@ -41,18 +52,33 @@ class ConstructorInjectionProvider<Type> implements ContextConfig.ComponentProvi
 
     @Override
     public List<Class<?>> getDependencies() {
-        return Stream.concat(stream(injectionConstructor.getParameterTypes()),
-            injectionFields.stream().map(Field::getType)).collect(Collectors.toList());
+        return concat(concat(stream(injectionConstructor.getParameterTypes()), injectionFields.stream().map(Field::getType)),
+            injectionMethods.stream().flatMap(m -> stream(m.getParameterTypes()))).collect(Collectors.toList());
     }
 
     private static <Type> List<Field> getInjectionFields(Class<Type> component) {
         List<Field> injectFields = new ArrayList<>();
         Class<Type> currentClass = component;
-        while(currentClass != Object.class) {
+        while (currentClass != Object.class) {
             injectFields.addAll(stream(currentClass.getDeclaredFields()).filter(f -> f.isAnnotationPresent(Inject.class)).toList());
             currentClass = (Class<Type>) currentClass.getSuperclass();
         }
         return injectFields;
+    }
+
+    private static <Type> List<Method> getInjectionMethods(Class<Type> component) {
+        List<Method> injectMethods = new ArrayList<>();
+        Class<Type> currentClass = component;
+        while (currentClass != Object.class) {
+            injectMethods.addAll(stream(currentClass.getDeclaredMethods()).filter(m -> m.isAnnotationPresent(Inject.class))
+                .filter(m -> injectMethods.stream().noneMatch(o -> o.getName().equals(m.getName()) && Arrays.equals(o.getParameterTypes(), m.getExceptionTypes()))).filter(
+                    m -> stream(component.getDeclaredMethods()).filter(m1 -> !m1.isAnnotationPresent(Inject.class))
+                        .noneMatch(o -> o.getName().equals(m.getName()) && Arrays.equals(o.getParameterTypes(), m.getExceptionTypes()))).toList());
+            currentClass = (Class<Type>) currentClass.getSuperclass();
+        }
+        //!!Important ensure the superclasses' methods are injected first
+        Collections.reverse(injectMethods);
+        return injectMethods;
     }
 
     private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
--- di.contanier/src/test/java/geektime/tdd/di/ContainerTest.java
+++ di.contanier/src/test/java/geektime/tdd/di/ContainerTest.java
@@ -191,7 +191,113 @@ public class ContainerTest {
 
         @Nested
         public class MethodInjection {
+            static class InjectMethodWithNoDependency {
+                boolean called = false;
 
+                @Inject
+                void install() {
+                    this.called = true;
+                }
+            }
+
+            //TODO  inject method with no dependencies will be called
+            @Test
+            public void should_call_inject_method_even_if_no_dependency_declared() {
+                config.bind(InjectMethodWithNoDependency.class, InjectMethodWithNoDependency.class);
+
+                InjectMethodWithNoDependency instance = config.getContext().get(InjectMethodWithNoDependency.class).get();
+
+                assertTrue(instance.called);
+            }
+
+            static class InjectMethodWithDependency {
+                Dependency dependency;
+
+                @Inject
+                void install(Dependency dependency) {
+                    this.dependency = dependency;
+                }
+            }
+
+            //TODO  inject method with dependencies will be injected
+            @Test
+            public void should_inject_dependency_via_inject_method() {
+                Dependency dependency = new Dependency() {
+                };
+                config.bind(Dependency.class, dependency);
+                config.bind(InjectMethodWithDependency.class, InjectMethodWithDependency.class);
+
+                InjectMethodWithDependency instance = config.getContext().get(InjectMethodWithDependency.class).get();
+                assertSame(dependency, instance.dependency);
+            }
+
+            //TODO  inject override inject method from superclass
+            static class SuperClassWithInjectMethod {
+                int superCalled = 0;
+
+                @Inject
+                void install() {
+                    superCalled++;
+                }
+            }
+
+            static class SubClassWithInjectMethod extends SuperClassWithInjectMethod {
+                int subCalled = 0;
+
+                @Inject
+                void installAnother() {
+                    subCalled = superCalled + 1;
+                }
+            }
+
+            @Test
+            public void should_inject_dependencies_via_inject_method_from_superclass() {
+                config.bind(SubClassWithInjectMethod.class, SubClassWithInjectMethod.class);
+
+                SubClassWithInjectMethod instance = config.getContext().get(SubClassWithInjectMethod.class).get();
+                assertEquals(1, instance.superCalled);
+                assertEquals(2, instance.subCalled);
+            }
+
+            static class SubClassOverrideSuperClassWithInject extends SuperClassWithInjectMethod {
+
+                @Inject
+                void install() {
+                    super.install();
+                }
+            }
+
+            @Test
+            public void should_only_call_once_if_subclass_override_superclass_inject_method_with_inject() {
+                config.bind(SubClassOverrideSuperClassWithInject.class, SubClassOverrideSuperClassWithInject.class);
+
+                SubClassOverrideSuperClassWithInject instance = config.getContext().get(SubClassOverrideSuperClassWithInject.class).get();
+                assertEquals(1, instance.superCalled);
+            }
+
+            static class SubClassOverrideSuperClassWithNoInject extends SuperClassWithInjectMethod {
+
+                void install() {
+                    super.install();
+                }
+            }
+
+            @Test
+            public void should_not_call_inject_method_if_override_with_no_inject() {
+                config.bind(SubClassOverrideSuperClassWithNoInject.class, SubClassOverrideSuperClassWithNoInject.class);
+
+                SubClassOverrideSuperClassWithNoInject instance = config.getContext().get(SubClassOverrideSuperClassWithNoInject.class).get();
+
+                assertEquals(0, instance.superCalled);
+            }
+
+            //TODO  include dependencies from inject methods
+            @Test
+            public void should_include_dependencies_from_inject_method() {
+                ConstructorInjectionProvider<InjectMethodWithDependency> provider = new ConstructorInjectionProvider<>(InjectMethodWithDependency.class);
+                assertArrayEquals(new Class<?>[] {Dependency.class}, provider.getDependencies().toArray());
+            }
+            //TODO  inject throw exception if type parameter defined
         }
 
     }
